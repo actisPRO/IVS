3df3b0f4f18151e1d84ca71ce4266d531862d7f5
Denis Karev <dkarev@protonmail.com>
2021-04-07 10:56:12 +0200

    better operations logic and keyboard controls

--- src/calc.cpp
+++ src/calc.cpp
@@ -27,6 +27,55 @@ Calc::~Calc()
     delete ui;
 }
 
+bool Calc::eventFilter(QObject *target, QEvent *event)
+{
+    if (target == ui->centralwidget)
+    {
+        if (event->type() == QEvent::KeyPress)
+        {
+            QKeyEvent *keyEvent = (QKeyEvent *) event;
+
+            if (keyEvent->key() == Qt::Key_0)
+                on_num_0_released();
+            else if (keyEvent->key() == Qt::Key_1)
+                on_num_1_released();
+            else if (keyEvent->key() == Qt::Key_2)
+                on_num_2_released();
+            else if (keyEvent->key() == Qt::Key_3)
+                on_num_3_released();
+            else if (keyEvent->key() == Qt::Key_4)
+                on_num_4_released();
+            else if (keyEvent->key() == Qt::Key_5)
+                on_num_5_released();
+            else if (keyEvent->key() == Qt::Key_6)
+                on_num_6_released();
+            else if (keyEvent->key() == Qt::Key_7)
+                on_num_7_released();
+            else if (keyEvent->key() == Qt::Key_8)
+                on_num_8_released();
+            else if (keyEvent->key() == Qt::Key_9)
+                on_num_9_released();
+            else if (keyEvent->key() == Qt::Key_Period || keyEvent->key() == Qt::Key_Comma)
+                on_num_dot_released();
+            else if (keyEvent->key() == Qt::Key_Plus)
+                on_op_add_released();
+            else if (keyEvent->key() == Qt::Key_Minus)
+                on_op_sub_released();
+            else if (keyEvent->key() == Qt::Key_Asterisk || keyEvent->key() == Qt::Key_multiply)
+                on_op_mult_released();
+            else if (keyEvent->key() == Qt::Key_Slash || keyEvent->key() == Qt::Key_division)
+                on_op_div_released();
+            else if (keyEvent->key() == Qt::Key_Equal || keyEvent->key() == Qt::Key_Enter)
+                on_op_eq_released();
+            else if (keyEvent->key() == Qt::Key_Backspace)
+                on_backspace_released();
+            else if (keyEvent->key() == Qt::Key_Delete)
+                on_clear_entry_released();
+        } // if (event->type() == QEvent::KeyPress)
+    } // if (target == ui->centralwidget)
+    return QWidget::eventFilter(target, event);
+}
+
 void Calc::addDigit(char digit)
 {
     if (ui->lineEdit->text() == "0" || waitingForInput)
@@ -72,6 +121,61 @@ double Calc::performCalculation(bool *ok)
                 showError("Cannot divide by zero");
                 return 0;
             }
+    }
+}
+
+void Calc::performOperation(OperationType nextOperation)
+{
+    // Means, that there was no input (eg. user pressed 5, then +, then -):
+    // user wants to change the operation
+    if (nextOperation != None && waitingForInput)
+    {
+        operation = nextOperation;
+        return;
+    }
+
+    waitingForInput = true;
+    bool ok = true;
+    if (!number1_set)
+    {
+        number1 = ui->lineEdit->text().toDouble(&ok);
+        if (!ok)
+        {
+            showError("Conversion error!");
+            return;
+        }
+        number1_set = true;
+        operation = nextOperation;
+    }
+    else
+    {
+        // If user presses = button more, then once, we want to repeat the latest 'normal'
+        // operation, using old number2 and displayed text, which is stored in number1 (as a result of the
+        // previous equation)
+        if (nextOperation == None && operation == None)
+        {
+            operation = previousOperation;
+        }
+        else
+        {
+            number2 = ui->lineEdit->text().toDouble(&ok);
+            if (!ok)
+            {
+                showError("Conversion error!");
+                return;
+            }
+        }
+
+        number1 = performCalculation(&ok);
+        if (!ok) return;
+
+        auto result = QString::asprintf("%.16g", number1);
+        ui->lineEdit->setText(result);
+
+        // If user presses the = button again, program will repeat the previous operation
+        // using the same number2, but with a new number1
+        previousOperation = operation;
+        operation = nextOperation;
     }
 }
 
@@ -149,6 +253,7 @@ void Calc::on_backspace_released()
 void Calc::on_clear_entry_released()
 {
     ui->lineEdit->setText("0");
+    waitingForInput = true;
 }
 
 void Calc::on_global_clear_released()
@@ -176,47 +281,6 @@ void Calc::on_op_sign_released()
     ui->lineEdit->setText(QString::number(input));
 }
 
-void Calc::performOperation(OperationType nextOperation)
-{
-    waitingForInput = true;
-
-    bool ok = true;
-    if (!number1_set)
-    {
-        // todo: error check
-        number1 = ui->lineEdit->text().toDouble(&ok);
-        if (!ok) return;
-        number1_set = true;
-        operation = nextOperation;
-    }
-    else
-    {
-        // If user presses = button more, then once, we want to repeat the latest 'normal'
-        // operation, using old number2 and displayed text, which is stored in number1 (as a result of the
-        // previous equation)
-        if (nextOperation == None && operation == None)
-        {
-            operation = previousOperation;
-        }
-        else
-        {
-            number2 = ui->lineEdit->text().toDouble(&ok);
-            if (!ok) return;
-        }
-
-        number1 = performCalculation(&ok);
-        if (!ok) return;
-
-        auto result = QString::asprintf("%.16g", number1);
-        ui->lineEdit->setText(result);
-
-        // If user presses the = button again, program will repeat the previous operation
-        // using the same number2, but with a new number1
-        previousOperation = operation;
-        operation = nextOperation;
-    }
-}
-
 void Calc::on_op_add_released()
 {
     performOperation(Addition);
@@ -241,3 +305,5 @@ void Calc::on_op_eq_released()
 {
     performOperation(None);
 }
+
+
